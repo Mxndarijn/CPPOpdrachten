7d3881550346ee65f04cb70ac9eda4d3d8b19b26
Merijn <mfg.gommeren@student.avans.nl>
2023-05-10 15:51:10 +0200

    finished w3 exercise 1

---
+++ W3Opdracht1/Course.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include "Course.h"
+
+using namespace std;
+
+Course::Course(const std::string& courseName, int capacity) {
+	numberOfStudents = 0;
+	this->courseName = courseName;
+	this->capacity = capacity;
+	students = new std::string[capacity];
+}
+
+Course::Course(const Course& course) {
+	numberOfStudents = course.getNumberOfStudents();
+	this->courseName = course.courseName;
+	this->capacity = course.capacity;
+	//this->students = course.students;
+	this->students = new std::string[this->capacity];
+	for (int i = 0; i < this->capacity; i++) {
+		this->students[i] = course.students[i];
+	}
+}
+
+Course::~Course() {
+	delete[] students;
+}
+
+std::string Course::getCourseName() const {
+	return courseName;
+}
+
+void Course::addStudent(const std::string& name) {
+	if (numberOfStudents >= capacity) {
+		int newCap = capacity + 1;
+		string* newStudents = new string[newCap];
+
+		for (int i = 0; i < capacity; i++) {
+			newStudents[i] = students[i];
+		}
+		delete[] students;
+
+		capacity = newCap;
+		students = newStudents;
+	}
+	students[numberOfStudents] = name;
+	numberOfStudents++;
+
+}
+
+void Course::dropStudent(const std::string& name) {
+	string* newStudents = new string[capacity];
+	int newNumberOfStudents = 0;
+
+	for (int i = 0; i < numberOfStudents; i++) {
+		if (students[i] != name) {
+			newStudents[newNumberOfStudents] = students[i];
+			newNumberOfStudents++;
+		}
+	}
+	numberOfStudents = newNumberOfStudents;
+	students = newStudents;
+}
+
+std::string* Course::getStudents() const {
+	return students;
+}
+
+int Course::getNumberOfStudents() const {
+	return numberOfStudents;
+}
+
+void Course::clear() {
+
+}
