2ae7b0d74e3d7a9fe981d79274a021c1615bbbaf
Merijn <mfg.gommeren@student.avans.nl>
2023-06-01 16:00:26 +0200

    Week 4 opdracht 1

---
+++ W4Opdracht1/Triangle.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Shape.h"
+
+class Triangle : public Shape {
+public:
+    Triangle();
+    Triangle(double side1, double side2, double side3);
+    double getSide1() const;
+    double getSide2() const;
+    double getSide3() const;
+    double getArea() const;
+    double getPerimeter() const;
+
+private:
+    double side1;
+    double side2;
+    double side3;
+};
